4c5cc35e1d8e26ebcff05be4d1299c758456defb
Ankitkurani1997 <ankitkurani76@gmail.com>
2023-02-14 15:45:05 +0530

    changes in CMAKE; added a test case in link analysis

---
+++ cpp/tests/link_analysis/pr-test.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include <cugraph/algorithms.hpp>
+#include <cugraph/graph.hpp>
+#include <cugraph/graph_functions.hpp>
+#include <cugraph/graph_view.hpp>
+using namespace std;
+
+
+class Pagerank {
+    public:
+    Pagerank() {
+
+    }
+    
+    void pagerank_uvm() {
+
+    }
+};
+
+
+int main() {
+    cout<<"Hello PR\n";
+    Pagerank pr;
+    pr.pagerank_uvm();
+}
